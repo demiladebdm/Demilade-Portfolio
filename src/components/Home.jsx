85dc57408528ed3ce4001cc98ab8e318f0c78114
Demilade Folarin <demiladefolarin@gmail.com>
2023-01-29 13:20:26 +0100

    minor refractor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'm a Full Stack Developer
+                    I'm a Front End Developer
                 </h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                     I have diverse set of skills and a proven track record of building seamless digital experiences. Proficient in all layers of development, ready to craft amazing websites and apps.
