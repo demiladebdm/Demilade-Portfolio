5df2beacc5e9fae10a18b385a8e3a643af307890
Demilade Folarin <demiladefolarin@gmail.com>
2023-01-30 09:00:56 +0100

    padding corrected

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
 
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row pt-80'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                     I'm a Front End Developer
@@ -29,7 +29,7 @@ const Home = () => {
             </div>
 
             <div>
-                <img src={HeroImage} alt="My profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                <img src={HeroImage} alt="My profile" className='rounded-2xl mx-auto w-2/3 md:w-full pb-40' />
             </div>
 
         </div>
